deed710475943ceb8dcd39949c1f05c42b8a1d08
Ali Tayarani <ali.tayarani@tapjoy.com>
2015-12-11 19:29:49 -0500

    missed a spec fix

--- spec/tapjoy/ldap/group/add_user_spec.rb
+++ spec/tapjoy/ldap/group/add_user_spec.rb
@@ -1,10 +1,9 @@
-require_relative '../../../spec_helper'
 require_relative '../group'
 describe 'Tapjoy::LDAP::Group::AddUser.add_user' do
   include_context 'group'
   let(:operations) {[[:add, :memberUid, 'test.user']]}
 
-  it 'creates a group' do
+  it 'adds a user to a group' do
     ARGV << %w(add_user -g testgroup -u test.user); ARGV.flatten!
     expect(fake_ldap).to receive(:modify).with(distinguished_name, operations)
     Tapjoy::LDAP::Group.commands
